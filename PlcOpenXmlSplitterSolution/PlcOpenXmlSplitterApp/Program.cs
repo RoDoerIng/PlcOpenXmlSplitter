0044ce24b1d4e4b2ad1e9a59ffd12034e418981a
RoDoerIng <ronnydoering.ing@gmail.com>
2017-07-24 22:33:19 +0200

    LOGGING NOT WORKING YET! -added log4net.config -added static logger to Program -edited App.config to use log4net

--- PlcOpenXmlSplitterSolution/PlcOpenXmlSplitterApp/Program.cs
+++ PlcOpenXmlSplitterSolution/PlcOpenXmlSplitterApp/Program.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Xml;
 using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using System.Reflection;
 
 namespace PlcOpenXmlSplitterApp
 {
     class Program
     {
+
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
+            XmlConfigurator.Configure();
+
+            log.Debug("Debug message");
+            log.Info("Info message");
+            log.Warn("Warning message");
+            log.Error("Error message");
+            log.Fatal("Fatal message");
         }
     }
 }
